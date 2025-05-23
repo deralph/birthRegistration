089b718dd5a58c2f8333bcecfe6392cdfaff0782
deralph <jraphael441@gmail.com>
2025-05-23 12:03:14 +0100

    corrected frontend login and register server link links

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -25,7 +25,8 @@ const Register = () => {
     try {
       await axios
         .post(
-          "http://localhost:5000/api/v1/user/patient/register",
+          // "http://localhost:5000/api/v1/user/patient/register",
+          "https://birthregistration.onrender.com/api/v1/user/patient/register",
           { firstName, lastName, email, phone, nic, dob, gender, password },
           {
             withCredentials: true,
